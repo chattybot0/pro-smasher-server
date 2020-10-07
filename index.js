1b8bbe046e01e465d8a06a9b4284da0ba8709325
chattybot0 <70719004+chattybot0@users.noreply.github.com>
2020-10-07 22:33:07 +0330

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,16 @@ io.on('connection', function (socket) {
 		io.emit("get", data);
 	});
 });
+app.use(express.static("public"))
 
+// define the first route
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>")
+})
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
 
 http.listen(3000, function() {
   console.log('listening on port 3000');
